d2dde8a4b561ec279b3a40f24644ee7d93c9719e
Nova <nova@novatea.dev>
2026-08-07 18:45:48 -0600

    initial work

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("java-library")
+    id("xyz.jpenilla.run-paper") version "3.0.2"
+    id("com.gradleup.shadow") version "9.4.1"
+}
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://jitpack.io")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
+    compileOnly("com.github.hackclub:HCCore:37b2603a15")
+    implementation("io.javalin:javalin:7.2.2")
+}
+
+java {
+    toolchain.languageVersion = JavaLanguageVersion.of(21)
+}
+
+tasks {
+    runServer {
+        // Configure the Minecraft version for our task.
+        // This is the only required configuration besides applying the plugin.
+        // Your plugin's jar (or shadowJar if present) will be used automatically.
+        minecraftVersion("1.21.11")
+        jvmArgs("-Xms2G", "-Xmx2G", "-Dcom.mojang.eula.agree=true")
+    }
+
+    processResources {
+        val props = mapOf("version" to version, "description" to project.description)
+        filesMatching("plugin.yml") {
+            expand(props)
+        }
+    }
+}
---
+++ src/main/java/com/hackclub/hccoreapi/HCCoreAPI.java
@@ -0,0 +1,17 @@
+package com.hackclub.hccoreapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HCCoreAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
